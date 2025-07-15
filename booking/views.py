5b160c98d6806d7435fea4a30a1d9a6e1d552c44
haixiZ11 <zhx_1996@126.com>
2025-07-15 15:33:56 +0800

    优化了主页的预约时间计算逻辑

--- booking/views.py
+++ booking/views.py
@@ -18,3 +18,9 @@ def admin(request):
 def room_status(request):
     """会议室状态视图"""
     return render(request, 'booking/room_status.html')
+
+@ensure_csrf_cookie
+def status_display(request):
+    """会议室状态显示屏视图"""
+    rooms = Room.objects.all()
+    return render(request, 'booking/status_display.html', {'rooms': rooms})
